d79011465965125e51da88a453c792a4be936ce8
Dmytrii Rakitenko <warusus@gmail.com>
2024-03-18 19:12:01 +0300

    Defined input and output functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def read_from_console():
+    pass
+
+
+def read_from_file(file_path):
+    pass
+
+
+def read_with_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def print_to_console(text):
+    pass
+
+
+def print_to_file(text, file_path):
+    pass
+
+
+def print_with_pandas(text, file_path):
+    pass
